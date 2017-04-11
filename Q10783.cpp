9c95479e7d6f53ea7b41ed4c6c5b4c8ba0cb1673
wootdylan3 <t30736@gmail.com>
2017-04-11 23:58:23 +0800

    Add Q10783 description.

---
+++ Q10783.cpp
@@ -0,0 +1,34 @@
+/******************************************/
+/* UVa 10783 Odd Sum                      */
+/* Author: WootDylan3                     */
+/* Date: 2017/04/11                       */
+/* Time Limit: 3 sec                      */
+/******************************************/
+
+
+/*
+Description
+Given a range [a, b], you are to find the summation of all the odd integers in this range. For example, the summation of all the odd integers in the range [3, 9] is
+3 + 5 + 7 + 9 = 24.
+
+Input
+There can be at multiple test cases. The first line of input gives you the number of test cases,T (1 ≤ T≤ 100). Then T test cases follow. Each test case consists of 2 integers a and b (0 a ≤ b ≤100) in two separate lines.
+
+Output
+For each test case you are to print one line of output - the serial number of the test case followed by the summation of the odd integers in the range [a, b].
+
+--------------------------------------------------------------------------------
+
+Sample Input  
+2
+1
+5
+3
+5
+
+Sample Output 
+Case 1: 9
+Case 2: 8
+
+--------------------------------------------------------------------------------
+*/
